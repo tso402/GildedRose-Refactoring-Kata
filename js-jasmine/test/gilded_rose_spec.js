c11172556c978c8e8203c7262e111101cd7fff51
James Ault <james.ault@johnlewis.co.uk>
2019-04-09 17:37:41 +0100

    Passing tests for a number of features

--- js-jasmine/test/gilded_rose_spec.js
+++ js-jasmine/test/gilded_rose_spec.js
@@ -18,4 +18,69 @@ describe("Gilded Rose", function() {
     expect(gildedRose.items[0].quality).toEqual(9);
     expect(gildedRose.items[0].sellIn).toEqual(9);
   })
+
+  it('should not decrease the sellIn or quality for Sulfuras', function(){
+    item2 = new Item('Sulfuras, Hand of Ragnaros', 10, 10)
+    gildedRose.addItem(item2)
+    gildedRose.updateQuality()
+    expect(gildedRose.items[1].quality).toEqual(10);
+    expect(gildedRose.items[1].sellIn).toEqual(10);
+  })
+
+  it('should increase the quality of Aged Brie as time progresses', function(){
+    item2 = new Item('Aged Brie', 10, 10)
+    gildedRose.addItem(item2)
+    gildedRose.updateQuality()
+    expect(gildedRose.items[1].quality).toEqual(11);
+    expect(gildedRose.items[1].sellIn).toEqual(9);
+  })
+
+  it('should not increase the quality an item above 50', function(){
+    item2 = new Item('Aged Brie', 10, 50)
+    gildedRose.addItem(item2)
+    gildedRose.updateQuality()
+    expect(gildedRose.items[1].quality).toEqual(50);
+    expect(gildedRose.items[1].sellIn).toEqual(9);
+  })
+
+  it('increase the quality of Backstage passes to a TAFKAL80ETC concert as time progresses', function(){
+    item2 = new Item('Backstage passes to a TAFKAL80ETC concert', 14, 10)
+    gildedRose.addItem(item2)
+    gildedRose.updateQuality()
+    expect(gildedRose.items[1].quality).toEqual(11);
+    expect(gildedRose.items[1].sellIn).toEqual(13);
+  })
+
+  it('increase the quality by 2 of Backstage passes to a TAFKAL80ETC concert as sellIn drops below 11', function(){
+    item2 = new Item('Backstage passes to a TAFKAL80ETC concert', 10, 10)
+    gildedRose.addItem(item2)
+    gildedRose.updateQuality()
+    expect(gildedRose.items[1].quality).toEqual(12);
+    expect(gildedRose.items[1].sellIn).toEqual(9);
+  })
+
+  it('increase the quality by 3 of Backstage passes to a TAFKAL80ETC concert as sellIn drops below 6', function(){
+    item2 = new Item('Backstage passes to a TAFKAL80ETC concert', 5, 10)
+    gildedRose.addItem(item2)
+    gildedRose.updateQuality()
+    expect(gildedRose.items[1].quality).toEqual(13);
+    expect(gildedRose.items[1].sellIn).toEqual(4);
+  })
+
+  it('reduces the quality of Backstage passes to a TAFKAL80ETC concert to 0 after sellIn date is passed', function(){
+    item2 = new Item('Backstage passes to a TAFKAL80ETC concert', 0, 10)
+    gildedRose.addItem(item2)
+    gildedRose.updateQuality()
+    expect(gildedRose.items[1].quality).toEqual(0);
+    expect(gildedRose.items[1].sellIn).toEqual(-1);
+  })
+
+  it('should not reduce quality below 0', function(){
+    item2 = new Item('Backstage passes to a TAFKAL80ETC concert', 0, 10)
+    gildedRose.addItem(item2)
+    gildedRose.updateQuality()
+    gildedRose.updateQuality()
+    expect(gildedRose.items[1].quality).toEqual(0);
+    expect(gildedRose.items[1].sellIn).toEqual(-2);
+  })
 });
